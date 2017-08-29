7d31dcc985d74cc3b83583a95062ed6d7073f17a
Kristiansax <krellesax@hotmail.com>
2017-08-29 23:07:13 +0200

    Add files via upload

---
+++ AuctionHouse/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AuctionHouse
+{
+    class Program
+    {
+        List<Auction> Auctions = new List<Auction>();
+        private void ClientThread(Socket klient)
+        {
+            NetworkStream stream = new NetworkStream(klient);
+            StreamReader reader = new StreamReader(stream);
+            StreamWriter writer = new StreamWriter(stream);
+            writer.AutoFlush = true;
+
+            IPEndPoint remoteIpEndPoint = klient.RemoteEndPoint as IPEndPoint;
+            IPEndPoint localIpEndPoint = klient.LocalEndPoint as IPEndPoint;
+
+            if (remoteIpEndPoint != null)
+            {
+                Console.WriteLine("I am connected to " + remoteIpEndPoint.Address + " on port number " + remoteIpEndPoint.Port);
+            }
+
+
+            writer.WriteLine("Ready");
+            while (true)
+            {
+                string input = reader.ReadLine();
+                switch (input.ToLower())
+                {
+                    case "test":
+                        writer.WriteLine("test returned");
+                        break;
+                    case "bid":
+                        writer.WriteLine();
+                        break;
+                    case "auctions":
+                        break;
+                }
+                }
+        }
+        static void Main(string[] args)
+        {
+            TcpListener server = new TcpListener(IPAddress.Any, 20001);
+            server.Start();
+            while (true)
+            {
+                while (true)
+                {
+                    Program program = new Program();
+                    Socket klient = server.AcceptSocket();
+                    Thread t = new Thread(() => program.ClientThread(klient));
+                    t.Start();
+
+                    //new Thread(program.ClientThread(klient)).Start(klient);
+                }
+            }
+        }
+
+
+
+
+    }
+}
---
+++ AuctionHouseClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionHouseClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient server = new TcpClient("localhost", 20001);
+            NetworkStream stream = server.GetStream();
+            StreamReader reader = new StreamReader(stream);
+            StreamWriter writer = new StreamWriter(stream);
+            writer.AutoFlush = true;
+
+            Console.WriteLine(reader.ReadLine());
+            while (true)
+            {
+                string input = Console.ReadLine();
+                writer.WriteLine(input);
+                string output = reader.ReadLine();
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
